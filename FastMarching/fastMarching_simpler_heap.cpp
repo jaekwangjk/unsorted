e0233b1a91ae1bf9d16b48c3fb30298c47f7dfd8
Jaekwang Kim <jaekwangkim@vpnpool-10-251-79-134.near.illinois.edu>
2023-03-25 15:06:35 -0500

    update before fixing

--- FastMarching/fastMarching_simpler_heap.cpp
+++ FastMarching/fastMarching_simpler_heap.cpp
@@ -4,36 +4,32 @@
 #include<cmath>
 #include<numeric>
 #include<limits>
-#include "simpler_heap.h"
-
-
+#include "simple_heap.h"
 #include "Matrix.h"
-
-
 #include<array> 
 #include<vector> 
+#include <fftw3.h>
 
 #define MAXVAL std::numeric_limits<double>::max()
 #define LARGEVAL 99998
 
-// Things to do : kill periodicity 
 
-// Applied....
-// [1] visited marker 
+// To be done... initial profile diffusion [done]
+
+// wall, visited, chi(grid), velocity로 변수 구분시키고 
+// 데이터 아웃 포맷 만들기 (MATLAB으로 즉각즉각 확인하게 좋도록...)
+// Fast Marching Paper 다시한번 읽어보기..
+
 // [2] velocity field 
 // [3] ...
-
 // Create a grid
 // Define a function chi on the grid
 // Input: grid size, velocity field, chi describing a surface, step size
 // Output: chi (t)
-//
-
 // Create a grid
 // Define a function chi on the grid
 // Define a surface, by initializing chi on all outer and inner grid points (w.r.t surface) to 0 and 1 respectively
 // (J.K.??????)
-
 // Tag the grid points as "alive", "close" (dead points that share a neighbor with alive points) and "far" points
 // Initialize u to zero at all alive points
 // loop 
@@ -42,149 +38,63 @@
 //     move the grid associated to the root to "alive"
 //     identify the "neighbors of the root" that are in "far", and "move" to "close" to form the new "close"
 // end
-// 
+
 // Input: grid size, velocity field, chi describing a surface, step size
 // Output: chi (t)
-//
+double eikonalUpdate_2d(double h1s, double h2s, double U1, double U2, double finverse); 
+double eikonalUpdate(int nx, int ny, int nz, int x, int y, int z, double *grid, double *velocity); 
 
+//Poisson equation FFT solver struct
+typedef struct  {
+  fftw_plan Forward;
+  fftw_plan Backward;
+  double *kernel;
 
-double eikonalUpdate_2d(double h1s, double h2s, double U1, double U2, double finverse)
-{
-  // conduct 2d eikonalUpdate, when discrim <0
-  // h1s,h2s: h1^2 where h1= 1.0/n1;
-  // U1,U2 : minimum value in 1 and 2 direction
-    
-  double result=0.0;
-  double alpha = h1s/h2s;
+  fftw_complex *workspace;
   
-  double a = h1s + h2s;
-  double b = -2.0 * (h1s*U1 + h2s*U2);
-  double c = h1s*U1*U1 + h2s*U2*U2 - h1s*h2s*pow(finverse,2);      
-  double discrim = b*b - 4*a*c;
-    
-  if(discrim>0)  {
-    result = (sqrt(b*b-4*a*c)-b)/(2*a);
-  }else if (U2 < U1)  {
-    result = U2 + sqrt(h2s) * finverse;
-  }else  {
-    result = U1 + sqrt(h1s) * finverse;
-  }
-  
-  return result;
-}
+}poissonSolver;
 
-double eikonalUpdate(int nx, int ny, int nz, int x, int y, int z, double *grid, double *velocity)
-{
-  double result =0.0;
-  //eikonal update in 3d with non-equal spacing for each direction
-  double hx=1/(nx*1.0); double hy=1/(ny*1.0); double hz=1/(nz*1.0);
-  
-  int index = z*ny*nx + y*nx + x;
-  
-  int x_west,x_east,y_south,y_north,z_down,z_up;
-  
-  x_west=fmax(x-1,0);
-  x_east=fmin(nx-1,x+1);
-  y_south=fmax(y-1,0);
-  y_north=fmin(ny-1,y+1);
-  z_down=fmax(z-1,0);
-  z_up=fmin(nz-1,z+1);  
-  
-  int west= z*ny*nx+ y*nx + x_west;
-  int east= z*ny*nx+ y*nx + x_east;
-  int south = z*ny*nx + y_south * nx + x;
-  int north = z*ny*nx + y_north * nx + x;
-  int down = z_down*ny*nx + y*nx + x;
-  int up = z_up*ny*nx + y*nx +x;
+//double initial_profile_new(const std::array<double,3>& x)
+//{
+//    double func =  20 * (x[0]-0.5)*(x[0]-0.5) + 30 * (x[1]-0.5)*(x[1]-0.5) - 1 ;
+//    return func; 
+//}
 
-  //f inverse in slowness
-  double finverse = 1.0/ velocity[index]; 
-  double tol = 1e-7;
-  
-  //testgrid is grid, true grid is indicator -- this is equivalent to upwind
-  double U_H=fmin(grid[west],grid[east]);
-  double U_V=fmin(grid[north],grid[south]);
-  double U_Z=fmin(grid[up], grid[down]);
-    
-  //We solve the quadratic equation for U_ij, current cell, using up wind
-  double hxs = hx*hx;
-  double hys = hy*hy;
-  double hzs = hz*hz;
-
-  double a = hxs*hys + hys*hzs + hxs*hzs;
-  double b = (-2.0) * (hys*hzs * U_H + hxs*hzs * U_V + hxs*hys* U_Z);
-  double c = hys*hzs*pow(U_H,2)+ hxs*hzs*pow(U_V,2) +
-                   hzs*hys*pow(U_Z,2)- hxs*hys*hzs*pow(finverse,2);
-    
-  double discrim = b*b - 4.0 *a *c;
-    
-  if(discrim>0) {
-    result=(sqrt(b*b-4.0*a*c)-b)/(2.0*a);
-  }else  {   //if discrim < 0,  then we take 2 dimensional updates in each plane
-        // and choose the smallest.
-    double result_xy;
-    double result_yz;
-    double result_zx;
-    
-    //in x-y plane, y-z, z-x plane separatedly
-    result_xy = eikonalUpdate_2d(hxs,hys,U_H,U_V,finverse);
-    result_yz = eikonalUpdate_2d(hys,hzs,U_V,U_Z,finverse);
-    result_zx = eikonalUpdate_2d(hzs,hxs,U_Z,U_H,finverse);
-    
-    // result=fmin(result_xy,result_yz);
-    // result=fmin(result, result_zx);
-    // just simply choose xy results
-	
-	result=result_xy;
-  }
-     
-     return result;
-}
-
-
-
-int initial_profile_new(const std::array<double,3>& x)
-{
-    double func =  20 * (x[0]-0.5)*(x[0]-0.5) + 30 * (x[1]-0.5)*(x[1]-0.5) - 1 ;
-    return (func <= 0.) ? 1 : 0;
-}
-
-void initialize_grid(int nx, int ny, int nz, double *chi, double *visited, const std::array<double,3>& corner, const double& h ){
+void initialize_wall(int nx, int ny, int nz, double *wall, double *visited, const std::array<double,3>& corner, const double& h ){
     std::array<double,3> x;
-    int boo;
-    std::ofstream chiFile;
-    chiFile.open("initialChi.txt");
-
+	
+	int pcount = nx*ny*nz; 
+		
+    std::ofstream wallFile;
+    wallFile.open("initialWall.txt");
+	
+    // Two step ---do profile first do other step later. 
     for(int iz= 0; iz< nz; iz++){ 
 	  x[2] = corner[2]+ iz*h;	
       for(int iy= 0; iy< ny; iy++){
 		x[1] = corner[1]+ iy*h ;  
 		  for(int ix =0; ix < nx; ix++){
-			 x[0] = corner[0]+ ix*h ; 
-			 boo = initial_profile_new(x); 
+		    x[0] = corner[0]+ ix*h ; 
 			 
-			 int indx = iz*nx*ny+ iy*nx + ix; 
-			 
-			 chi[indx] = (boo==1) ? -0.1 : LARGEVAL; 
-			 
-			 //Mark the starting point 
-			 if(ix==5 && iy ==5){
-				chi[indx] = 0.0; 
-				std::cout<<"Starting point has beend read\n" << std::endl; 
-			 }
-			 
-			//mark visited cell...
-			//either initial starting point or obstacles...
+			int indx = iz*nx*ny+ iy*nx + ix; 
+			double func =  20 * (x[0]-0.5)*(x[0]-0.5) + 30 * (x[1]-0.5)*(x[1]-0.5) - 1 ;
 			
-			double tol =1e-7;
-			if( chi[indx]-0.0 < tol){
-			visited[indx]=1.0; 
-		    }
+			if(func < 0 ) 
+			object[indx] = 1.0; 
+			else
+			object[indx] = 0.0; 
 			
-			chiFile << x[0] << ", " << x[1]  << ", " << chi[indx] << std::endl; 	  
-			  
+			wallFile << x[0] << ", " << x[1]  << ", " << chi[indx] << std::endl;
 	}}}
-    chiFile.close();
+	
+    for(int i=0; i< pcount; i++){	
+	  visited[i]=0.0; //initialize 
+		 
+	  if( fabs(wall[indx]-1.0) < tol)
+	   visited[indx]=1.0; 	 
+	}  
+			 
+    wallFile.close();
 }
 
 
@@ -193,6 +103,56 @@ void initialize_velocity(int nx, int ny, int nz, double *chi, double *velocity){
 	
 	double smallValue = 1e-9;
     
+	// smoothing obstacles
+	poissonSolver fftps; /* The"workspace"-stucture */
+	int pcount = nx*ny*nz; 
+	fftps.workspace= new fftw_complex[pcount]();
+	fftps.kernel = new double[pcount]();
+	fftps.Forward=fftw_plan_dft_3d(nz,ny,nx,fftps.workspace,fftps.workspace,
+		                                       -1,FFTW_MEASURE);
+	fftps.Backward=fftw_plan_dft_3d(nz,ny,nx,fftps.workspace,fftps.workspace,
+		                                        1, FFTW_MEASURE);
+	// prepare kernel...
+	for(int iz= 0; iz< nz; iz++){ 
+	 for(int iy= 0; iy< ny; iy++){
+	  for(int ix =0; ix < nx; ix++){
+	    double xk = 2*M_PI*ix/(nx*1.0);
+	    double yk = 2*M_PI*iy/(ny*1.0);
+		double zk = 2*M_PI*iz/(nz*1.0);
+		double Eigenvalues=2*nx*nx*(1-cos(xk))+2*ny*ny*(1-cos(yk))+2*nz*nz*(1-cos(zk));
+		fftps.kernel[iz*ny*nx+ iy*nx+ix]=Eigenvalues;
+    }}}
+	
+	std::cout<<"I am here! A4 !" << std::endl; 
+
+    std::cout<<"I am here! B !" << std::endl; 
+	
+	for(int i=0;i<pcount;i++){
+		fftps.workspace[i][0]=chi[i];
+		fftps.workspace[i][1]=0.0;
+	}	
+	
+	fftw_execute(fftps.Forward);
+	
+	std::cout<<"I am here! C !" << std::endl; 
+
+	double dt = 0.01; 
+	
+	for(int i=0;i<pcount;i++){
+	    fftps.workspace[i][0]*=exp(-fftps.kernel[i]*dt)/pcount;
+	    fftps.workspace[i][1]*=exp(-fftps.kernel[i]*dt)/pcount;
+	}	
+	
+	fftw_execute(fftps.Backward);
+	
+	std::cout<<"I am here! D !" << std::endl; 
+	
+	for(int i=0;i<pcount;i++){
+		chi[i]=fftps.workspace[i][0]; 
+	}	
+	
+	
+	
 	for(int iz= 0; iz< nz; iz++){ 
       for(int iy= 0; iy< ny; iy++){ 
 		  for(int ix =0; ix < nx; ix++){
@@ -228,13 +188,6 @@ int main()
     // total number of grid points
     int pcount = nx*ny*nz;
 
-    // Movie
-    std::string str = "ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt gray -s " + std::to_string(nx) + 
-                      "x" + std::to_string(ny) + " -r 30 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 out.mp4";
-	
-    FILE* pipeout = popen(str.c_str(), "w");
-    unsigned char* pixels = new unsigned char[pcount];
-    
 	double *chi; 
 	double *visited; 
 	double *velocity; 
@@ -244,10 +197,19 @@ int main()
 	visited = new double[pcount]();
 	velocity = new double[pcount]();
 	
+	
 	// This is input!! 
 	//Matrix<dim> velocity(nx,ny,nz);
     initialize_grid(nx,ny,nz, chi,visited,corner,h);
 	initialize_velocity(nx,ny,nz,chi,velocity);
+	
+    // For Movie
+    //std::string str = "ffmpeg -y -f rawvideo -vcodec rawvideo -pix_fmt gray -s " + std::to_string(nx) + 
+	//                  "x" + std::to_string(ny) + " -r 30 -i - -f mp4 -q:v 5 -an -vcodec mpeg4 out.mp4";
+	
+    //FILE* pipeout = popen(str.c_str(), "w");
+    unsigned char* pixels = new unsigned char[pcount];
+    
 	
 	i_heap heap;
 	heap = i_heap_create_empty_heap(pcount, pcount);
@@ -354,10 +316,7 @@ int main()
 
     std::ofstream closeFile;
     closeFile.open("closeChi.txt");
- 
-	
-
-	
+ 	
     for(int iz= 0; iz< nz; iz++){ 
 	  x[2] = corner[2]+ iz*h;	
       for(int iy= 0; iy< ny; iy++){
@@ -365,19 +324,109 @@ int main()
 		  for(int ix =0; ix < nx; ix++){
 			 x[0] = corner[0]+ ix*h ; 	
 			 int indx = iz*nx*ny+ iy*nx + ix; 
-			 closeFile << x[0] << ", " << x[1]  << ", " << chi[indx] << std::endl;	  	  
+			 closeFile << x[0] << " " << x[1]  << " " << chi[indx] << std::endl;	  	  
 	}}}
 	
-
-
-
-
-
-   fflush(pipeout);
-   pclose(pipeout);
-   delete[] pixels;
+   //fflush(pipeout);
+   //pclose(pipeout);
+   //delete[] pixels;
     
-    return 0;
+   return 0;
 }
 
 
+
+// detailed functions 
+
+double eikonalUpdate_2d(double h1s, double h2s, double U1, double U2, double finverse)
+{
+  // conduct 2d eikonalUpdate, when discrim <0
+  // h1s,h2s: h1^2 where h1= 1.0/n1;
+  // U1,U2 : minimum value in 1 and 2 direction
+    
+  double result=0.0;
+  double alpha = h1s/h2s;
+  
+  double a = h1s + h2s;
+  double b = -2.0 * (h1s*U1 + h2s*U2);
+  double c = h1s*U1*U1 + h2s*U2*U2 - h1s*h2s*pow(finverse,2);      
+  double discrim = b*b - 4*a*c;
+    
+  if(discrim>0)  {
+    result = (sqrt(b*b-4*a*c)-b)/(2*a);
+  }else if (U2 < U1)  {
+    result = U2 + sqrt(h2s) * finverse;
+  }else  {
+    result = U1 + sqrt(h1s) * finverse;
+  }
+  
+  return result;
+}
+
+double eikonalUpdate(int nx, int ny, int nz, int x, int y, int z, double *grid, double *velocity)
+{
+  double result =0.0;
+  //eikonal update in 3d with non-equal spacing for each direction
+  double hx=1/(nx*1.0); double hy=1/(ny*1.0); double hz=1/(nz*1.0);
+  
+  int index = z*ny*nx + y*nx + x;
+  
+  int x_west,x_east,y_south,y_north,z_down,z_up;
+  
+  x_west=fmax(x-1,0);
+  x_east=fmin(nx-1,x+1);
+  y_south=fmax(y-1,0);
+  y_north=fmin(ny-1,y+1);
+  z_down=fmax(z-1,0);
+  z_up=fmin(nz-1,z+1);  
+  
+  int west= z*ny*nx+ y*nx + x_west;
+  int east= z*ny*nx+ y*nx + x_east;
+  int south = z*ny*nx + y_south * nx + x;
+  int north = z*ny*nx + y_north * nx + x;
+  int down = z_down*ny*nx + y*nx + x;
+  int up = z_up*ny*nx + y*nx +x;
+
+  //f inverse in slowness
+  double finverse = 1.0/velocity[index]; 
+  double tol = 1e-7;
+  
+  //testgrid is grid, true grid is indicator -- this is equivalent to upwind
+  double U_H=fmin(grid[west],grid[east]);
+  double U_V=fmin(grid[north],grid[south]);
+  double U_Z=fmin(grid[up], grid[down]);
+    
+  //We solve the quadratic equation for U_ij, current cell, using up wind
+  double hxs = hx*hx;
+  double hys = hy*hy;
+  double hzs = hz*hz;
+
+  double a = hxs*hys + hys*hzs + hxs*hzs;
+  double b = (-2.0) * (hys*hzs * U_H + hxs*hzs * U_V + hxs*hys* U_Z);
+  double c = hys*hzs*pow(U_H,2)+ hxs*hzs*pow(U_V,2) +
+                   hzs*hys*pow(U_Z,2)- hxs*hys*hzs*pow(finverse,2);
+    
+  double discrim = b*b - 4.0 *a *c;
+    
+  if(discrim>0) {
+    result=(sqrt(b*b-4.0*a*c)-b)/(2.0*a);
+  }else  {   //if discrim < 0,  then we take 2 dimensional updates in each plane
+        // and choose the smallest.
+    double result_xy;
+    double result_yz;
+    double result_zx;
+    
+    //in x-y plane, y-z, z-x plane separatedly
+    result_xy = eikonalUpdate_2d(hxs,hys,U_H,U_V,finverse);
+    result_yz = eikonalUpdate_2d(hys,hzs,U_V,U_Z,finverse);
+    result_zx = eikonalUpdate_2d(hzs,hxs,U_Z,U_H,finverse);
+    
+    // result=fmin(result_xy,result_yz);
+    // result=fmin(result, result_zx);
+    // just simply choose xy results
+	
+	result=result_xy;
+  }
+     
+     return result;
+}
